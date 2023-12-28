bf4d219be88b2596f9691686292e44d63300fc4a
Marcel Juenemann <git@marcel.world>
2023-12-28 21:50:29 +0100

    Calculate historic returns from historic prices

--- webapp/src/app/calc/simulation.service.ts
+++ webapp/src/app/calc/simulation.service.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
 
-import * as HISTORIC_PRICES from './us-real-returns-since-1871.json';
+import { HISTORIC_PRICES } from './historic-us-returns';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SimulationService {
 
-  constructor() { }
+  private historicReturns: number[] = []
+
+  constructor() { 
+    for (let i = 12; i < HISTORIC_PRICES.length; i++) {
+      this.historicReturns.push(HISTORIC_PRICES[i] / HISTORIC_PRICES[i - 12])
+    }
+  }
 
   public foo(): any {
-    return HISTORIC_PRICES
+    let sum = 0.0
+    let histogram = new Map<number, number>()
+    for (let r of this.historicReturns) {
+      sum += r
+      const bucket = (Math.floor(r * 10) - 10) * 10
+      console.log("Bucket", bucket, r)
+      histogram.set(bucket, (histogram.get(bucket) || 0) + 1)
+    }
+    for (let [key, count] of histogram) {
+      console.log(key, count)
+    }
+    return sum / this.historicReturns.length
   } 
 }
